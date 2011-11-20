edc0b6738bdcdbb19ac44472d43faa91137a921d
Wei Li <weili.mail@gmail.com>
2011-11-20 17:40:44 -0500

    Rename and add test

--- src/com/test/groovy/MOPTest.groovy
+++ src/com/test/groovy/MOPTest.groovy
@@ -1,31 +1,5 @@
 package com.test.groovy
 
-import groovy.xml.MarkupBuilder
-
-
-def xml = new MarkupBuilder()
-println "-----Create XML using MarkupBuilder"
-xml.people {
-	person (id: "1"){
-		name "fn1 ln1"
-		age 22
-	}
-	person (id: "2", gender: "Male"){
-		name "fn2 ln2"
-		age: 33
-	}
-	person (id: "3"){
-		name "fn3 ln3"
-		age 33
-		address (){
-			street "56 Dream st"
-			city "Boston"
-			zip 23456
-		}
-	}
-}
-
-
 println "----- Define Dog class"
 class Dog{
 }
